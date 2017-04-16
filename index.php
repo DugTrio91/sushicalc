b259d4c6edb9156d192a3fded7d0f1e6e29bd72d
Mark Allen <markdallen@me.com>
2017-04-16 19:34:02 +0100

    Started JS calc

--- index.php
+++ index.php
@@ -1,10 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Sushicalc</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/main.css"> 
 </head>
+
 <body>
-    
+    <div class="sushi-prices">
+        <div class="sushi-button" id="green-plate">
+            <span onclick="greenMinus()" id="gMinus" class="minus-plate"><i class="ion-minus-round"></i></span>
+            <span class="price">£1.90</span>
+            <input type="text" value="0" id="green-total" class="tally"></input>
+            <span onclick="greenAdd()" class="plus-plate"><i class="ion-plus-round"></i></span>
+        </div>
+        <div class="sushi-button" id="blue-plate">
+            <span onclick="blueMinus()" id="bMinus" class="minus-plate"><i class="ion-minus-round"></i></span>
+            <span class="price">£2.50</span>
+            <input type="text" value="0" id="blue-total" class="tally"></input>
+            <span onclick="blueAdd()" class="plus-plate"><i class="ion-plus-round"></i></span>
+        </div>
+        <div class="sushi-button" id="purple-plate">
+            <span onclick="purpleMinus()" id="puMinus" class="minus-plate"><i class="ion-minus-round"></i></span>
+            <span class="price">£3.10</span>
+            <input type="text" value="0" id="purple-total" class="tally"></input>
+            <span onclick="purpleAdd()" id="gMinus" class="plus-plate"><i class="ion-plus-round"></i></span>
+        </div>
+        <div class="sushi-button" id="orange-plate">
+            <span onclick="orangeMinus()" id="oMinus" class="minus-plate"><i class="ion-minus-round"></i></span>
+            <span class="price">£3.60</span>
+            <input type="text" value="0" id="orange-total" class="tally"></input>
+            <span onclick="orangeAdd()" id="gMinus" class="plus-plate"><i class="ion-plus-round"></i></span>
+        </div>
+        <div class="sushi-button" id="pink-plate">
+            <span onclick="pinkMinus()" id="pMinus" class="minus-plate"><i class="ion-minus-round"></i></span>
+            <span class="price">£4.10</span>
+            <input type="text" value="0" id="pink-total" class="tally"></input>
+            <span onclick="pinkAdd()" class="plus-plate"><i class="ion-plus-round"></i></span>
+        </div>
+        <div class="sushi-button" id="grey-plate">
+            <span onclick="greyMinus()" id="grMinus" class="minus-plate"><i class="ion-minus-round"></i></span>
+            <span class="price">£5.00</span>
+            <input type="text" value="0" id="grey-total" class="tally"></input>
+            <span onclick="greyAdd()" class="plus-plate"><i class="ion-plus-round"></i></span>
+        </div>
+        <div class="sushi-button" id="yellow-plate">
+            <span onclick="yellowMinus()" id="yMinus" class="minus-plate"><i class="ion-minus-round"></i></span>
+            <span class="price">£6.00</span>
+            <input type="text" value="0" id="yellow-total" class="tally"></input>
+            <span onclick="yellowAdd()" class="plus-plate"><i class="ion-plus-round"></i></span>
+        </div>
+    </div>
+    <div class="sushi-total">
+        <input type="text" id="total" class="total" value="0"></input>
+    </div>
 </body>
+<script>
+
+    var i = 0;
+    
+    var greenTotal = document.getElementById('green-total');
+    var blueTotal = document.getElementById('blue-total');
+    var purpleTotal = document.getElementById('purple-total');
+    var orangeTotal = document.getElementById('orange-total');
+    var pinkTotal = document.getElementById('pink-total');
+    var greyTotal = document.getElementById('grey-total');
+    var yellowTotal = document.getElementById('yellow-total');
+    
+    function greenAdd() {
+        greenTotal.value = ++i;
+        if (greenTotal.value > 0){
+                document.getElementById('gMinus').style.visibility = 'visible';
+                greenTotal.style.visibility = 'visible';
+            }
+    }
+    function greenMinus() {
+        greenTotal.value = --i;
+        if (greenTotal.value <= 0){
+                document.getElementById('gMinus').style.visibility = 'hidden';
+                greenTotal.style.visibility = 'hidden';
+            }
+    }
+    
+    function blueAdd() {
+        blueTotal.value = ++i;
+        if (blueTotal.value > 0){
+                document.getElementById('bMinus').style.visibility = 'visible';
+                blueTotal.style.visibility = 'visible';
+            }
+    }
+    function blueMinus() {
+        blueTotal.value = --i;
+        if (blueTotal.value <= 0){
+                document.getElementById('bMinus').style.visibility = 'hidden';
+                blueTotal.style.visibility = 'hidden';
+            }
+    }
+    
+    
+    
+    
+    
+    
+    
+    function totalSum() {
+        var arr = document.getElementsByName('tally');
+        
+    }
+    
+    
+    
+    
+</script>
+        
+    
+    
+</script>
+
 </html>
