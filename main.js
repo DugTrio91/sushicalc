90736a17cce83a17af4cdbd98a39185a5a556f38
Mark Allen <markdallen@me.com>
2017-04-17 21:52:13 +0100

    calculation working

--- main.js
+++ main.js
@@ -4,19 +4,23 @@ var totalBillBox = document.getElementById('total');
 var greenTotal = 0;
 var greenCost = 0;
 var greenTotalBox = document.getElementById('green-total');
-greenCost.addEventListener("click", calculateTotal);
 var blueTotal = 0;
 var blueCost = 0;
 var blueTotalBox = document.getElementById('blue-total');
 var purpleTotal = 0;
+var purpleCost = 0;
 var purpleTotalBox = document.getElementById('purple-total');
 var orangeTotal = 0;
+var orangeCost = 0;
 var orangeTotalBox = document.getElementById('orange-total');
 var pinkTotal = 0;
+var pinkCost = 0;
 var pinkTotalBox = document.getElementById('pink-total');
 var greyTotal = 0;
+var greyCost = 0;
 var greyTotalBox = document.getElementById('grey-total');
 var yellowTotal = 0;
+var yellowCost = 0;
 var yellowTotalBox = document.getElementById('yellow-total');
 
     //green plate function
@@ -24,6 +28,7 @@ var yellowTotalBox = document.getElementById('yellow-total');
         greenTotal++;
         greenTotalBox.value = greenTotal;
         greenCost = greenTotal * 1.90;
+        calculateTotal();
     }
     
     function greenMinus() {
@@ -31,6 +36,7 @@ var yellowTotalBox = document.getElementById('yellow-total');
             greenTotal--;
             greenTotalBox.value = greenTotal;
             greenCost = greenTotal * 1.90;
+            calculateTotal();
             }
     }
 
@@ -39,6 +45,7 @@ var yellowTotalBox = document.getElementById('yellow-total');
         blueTotal++;
         blueTotalBox.value = blueTotal;
         blueCost = blueTotal * 2.50;
+        calculateTotal();
     }
     
     function blueMinus() {
@@ -46,6 +53,7 @@ var yellowTotalBox = document.getElementById('yellow-total');
             blueTotal--;
             blueTotalBox.value = blueTotal;
             blueCost = blueTotal * 2.50;
+            calculateTotal();
             }
     }
 
@@ -53,12 +61,16 @@ var yellowTotalBox = document.getElementById('yellow-total');
     function purpleAdd(){
         purpleTotal++;
         purpleTotalBox.value = purpleTotal;
+        purpleCost = purpleTotal * 3.10;
+        calculateTotal();
     }
     
     function purpleMinus() {
         if (purpleTotal > 0){
             purpleTotal--;
             purpleTotalBox.value = purpleTotal;
+            purpleCost = purpleTotal * 3.10;
+            calculateTotal();
             }
     }
 
@@ -66,12 +78,16 @@ var yellowTotalBox = document.getElementById('yellow-total');
     function orangeAdd(){
         orangeTotal++;
         orangeTotalBox.value = orangeTotal;
+        orangeCost = orangeTotal * 3.60;
+        calculateTotal();
     }
     
     function orangeMinus() {
         if (orangeTotal > 0){
             orangeTotal--;
             orangeTotalBox.value = orangeTotal;
+            orangeCost = orangeTotal * 3.60;
+            calculateTotal();
             }
     }
 
@@ -79,12 +95,16 @@ var yellowTotalBox = document.getElementById('yellow-total');
     function pinkAdd(){
         pinkTotal++;
         pinkTotalBox.value = pinkTotal;
+        pinkCost = pinkTotal * 4.10;
+        calculateTotal();
     }
     
     function pinkMinus() {
         if (pinkTotal > 0){
             pinkTotal--;
             pinkTotalBox.value = pinkTotal;
+            pinkCost = pinkTotal * 4.10;
+            calculateTotal();
             }
     }
 
@@ -92,12 +112,16 @@ var yellowTotalBox = document.getElementById('yellow-total');
     function greyAdd(){
         greyTotal++;
         greyTotalBox.value = greyTotal;
+        greyCost = greyTotal * 5.00;
+        calculateTotal();
     }
     
     function greyMinus() {
         if (greyTotal > 0){
             greyTotal--;
             greyTotalBox.value = greyTotal;
+            greyCost = greyTotal * 5.00;
+            calculateTotal();
             }
     }
 
@@ -105,18 +129,20 @@ var yellowTotalBox = document.getElementById('yellow-total');
     function yellowAdd(){
         yellowTotal++;
         yellowTotalBox.value = yellowTotal;
+        yellowCost = yellowTotal * 6.00;
+        calculateTotal();
     }
     
     function yellowMinus() {
         if (yellowTotal > 0){
             yellowTotal--;
             yellowTotalBox.value = yellowTotal;
+            yellowCost = yellowTotal * 6.00;
+            calculateTotal();
             }
     }
 
-
-
    function calculateTotal() {
-        totalBill = greenCost + blueCost;
-        totalBillBox.vlaue = totalBill;
+        totalBill = greenCost + blueCost + purpleCost + orangeCost + pinkCost + greyCost + yellowCost;
+        totalBillBox.value = £ + totalBill;
    }
