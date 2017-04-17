71877b288b9953b1f2a270fe13a35c712dfdb44c
Mark Allen <markdallen@me.com>
2017-04-17 17:49:30 +0100

    plates selected working

--- main.js
+++ main.js
@@ -1,50 +1,122 @@
-var i = 0;
-    
-    var greenTotal = document.getElementById('green-total');
-    var blueTotal = document.getElementById('blue-total');
-    var purpleTotal = document.getElementById('purple-total');
-    var orangeTotal = document.getElementById('orange-total');
-    var pinkTotal = document.getElementById('pink-total');
-    var greyTotal = document.getElementById('grey-total');
-    var yellowTotal = document.getElementById('yellow-total');
-    
-    function greenAdd() {
-        greenTotal.value = ++i;
-        if (greenTotal.value > 0){
-                document.getElementById('gMinus').style.visibility = 'visible';
-                greenTotal.style.visibility = 'visible';
-            }
+var totalBill = 0;
+var totalBillBox = document.getElementById('total');
+
+var greenTotal = 0;
+var greenCost = 0;
+var greenTotalBox = document.getElementById('green-total');
+greenCost.addEventListener("click", calculateTotal);
+var blueTotal = 0;
+var blueCost = 0;
+var blueTotalBox = document.getElementById('blue-total');
+var purpleTotal = 0;
+var purpleTotalBox = document.getElementById('purple-total');
+var orangeTotal = 0;
+var orangeTotalBox = document.getElementById('orange-total');
+var pinkTotal = 0;
+var pinkTotalBox = document.getElementById('pink-total');
+var greyTotal = 0;
+var greyTotalBox = document.getElementById('grey-total');
+var yellowTotal = 0;
+var yellowTotalBox = document.getElementById('yellow-total');
+
+    //green plate function
+    function greenAdd(){
+        greenTotal++;
+        greenTotalBox.value = greenTotal;
+        greenCost = greenTotal * 1.90;
     }
+    
     function greenMinus() {
-        greenTotal.value = --i;
-        if (greenTotal.value <= 0){
-                document.getElementById('gMinus').style.visibility = 'hidden';
-                greenTotal.style.visibility = 'hidden';
+        if (greenTotal > 0){
+            greenTotal--;
+            greenTotalBox.value = greenTotal;
+            greenCost = greenTotal * 1.90;
             }
+    }
+
+    //blue plate function
+    function blueAdd(){
+        blueTotal++;
+        blueTotalBox.value = blueTotal;
+        blueCost = blueTotal * 2.50;
     }
     
-    function blueAdd() {
-        blueTotal.value = ++i;
-        if (blueTotal.value > 0){
-                document.getElementById('bMinus').style.visibility = 'visible';
-                blueTotal.style.visibility = 'visible';
-            }
-    }
     function blueMinus() {
-        blueTotal.value = --i;
-        if (blueTotal.value <= 0){
-                document.getElementById('bMinus').style.visibility = 'hidden';
-                blueTotal.style.visibility = 'hidden';
+        if (blueTotal > 0){
+            blueTotal--;
+            blueTotalBox.value = blueTotal;
+            blueCost = blueTotal * 2.50;
             }
     }
-    
-    
-    
-    
-    
-    
-    
-    function totalSum() {
-        var arr = document.getElementsByName('tally');
-        
+
+    //purple plate function
+    function purpleAdd(){
+        purpleTotal++;
+        purpleTotalBox.value = purpleTotal;
     }
+    
+    function purpleMinus() {
+        if (purpleTotal > 0){
+            purpleTotal--;
+            purpleTotalBox.value = purpleTotal;
+            }
+    }
+
+    //orange plate function
+    function orangeAdd(){
+        orangeTotal++;
+        orangeTotalBox.value = orangeTotal;
+    }
+    
+    function orangeMinus() {
+        if (orangeTotal > 0){
+            orangeTotal--;
+            orangeTotalBox.value = orangeTotal;
+            }
+    }
+
+    //pink plate function
+    function pinkAdd(){
+        pinkTotal++;
+        pinkTotalBox.value = pinkTotal;
+    }
+    
+    function pinkMinus() {
+        if (pinkTotal > 0){
+            pinkTotal--;
+            pinkTotalBox.value = pinkTotal;
+            }
+    }
+
+    //grey plate function
+    function greyAdd(){
+        greyTotal++;
+        greyTotalBox.value = greyTotal;
+    }
+    
+    function greyMinus() {
+        if (greyTotal > 0){
+            greyTotal--;
+            greyTotalBox.value = greyTotal;
+            }
+    }
+
+    //yellow plate function
+    function yellowAdd(){
+        yellowTotal++;
+        yellowTotalBox.value = yellowTotal;
+    }
+    
+    function yellowMinus() {
+        if (yellowTotal > 0){
+            yellowTotal--;
+            yellowTotalBox.value = yellowTotal;
+            }
+    }
+
+
+
+   function calculateTotal() {
+        totalBill = greenCost + blueCost;
+        totalBillBox.vlaue = totalBill;
+   }
